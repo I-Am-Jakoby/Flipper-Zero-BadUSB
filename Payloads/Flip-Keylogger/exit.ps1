f5b89de2e6eda78c603b2b851753a8264521a0a9
I-Am-Jakoby <Michaelryanchitwood@gmail.com>
2022-12-24 22:56:20 -0600

    Create exit.ps1

---
+++ Payloads/Flip-Keylogger/exit.ps1
@@ -0,0 +1,3 @@
+Remove-Item "$env:appdata\-locker" -Recurse -Force;
+Remove-Item "$env:userprofile\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\-p.cmd"
+Remove-Item "$env:appdata\-exit.ps1"
