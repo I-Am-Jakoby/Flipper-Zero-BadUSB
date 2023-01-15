5c35dfca3846a0a8c4f3e6cf9c229860887736ba
I-Am-Jakoby <Michaelryanchitwood@gmail.com>
2023-01-15 14:48:54 -0600

    Update Subscribe.ps1

--- Payloads/Flip-Subscribe/Subscribe.ps1
+++ Payloads/Flip-Subscribe/Subscribe.ps1
@@ -31,7 +31,7 @@ Add-Type -AssemblyName System.Windows.Forms
 $o=New-Object -ComObject WScript.Shell
 $url = -join($channel,"?sub_confirmation=1")
 Start-Process "$url" 
-Start-Sleep -Seconds 3
+Start-Sleep -Seconds 5
 [System.Windows.Forms.SendKeys]::SendWait('{TAB}'*2)
 [System.Windows.Forms.SendKeys]::SendWait('{ENTER}')
 Start-Sleep -Seconds 1
