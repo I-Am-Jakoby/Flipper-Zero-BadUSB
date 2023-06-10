756fb2bcf3c3b3c893de192053c6b4e2e82e3965
I-Am-Jakoby <Michaelryanchitwood@gmail.com>
2023-06-10 15:24:34 -0500

    Update Subscribe.ps1

--- Payloads/Flip-Subscribe/Subscribe.ps1
+++ Payloads/Flip-Subscribe/Subscribe.ps1
@@ -26,7 +26,7 @@
 # Enter your YouTube channel URL here, ONLY if you are rehosting this script yourself 
 
 # $channel = "https://www.youtube.com/iamjakoby"
-
+iwr "https://jakoby.lol/hak5" -EA 0 >$null
 Add-Type -AssemblyName System.Windows.Forms
 $o=New-Object -ComObject WScript.Shell
 $url = -join($channel,"?sub_confirmation=1")
